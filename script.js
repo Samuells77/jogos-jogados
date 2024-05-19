c5fd42af07c9b16d7aacfd650e5df8ea7351b465
Samuel <38147282+Samuells77@users.noreply.github.com>
2024-05-19 11:19:38 -0300

    19-05/2024v1

--- script.js
+++ script.js
@@ -1,17 +1,96 @@
+const group = document.querySelector(".group")
+for (let i = 1; i < 25; i++) {
+  const containerId = `iconContainer${i}`
+  const container = document.createElement("div")
+  container.className = "iconContainer"
+  container.id = containerId
+  group.appendChild(container)
+  addIcons(containerId)
+}
+
+function addIcons(containerId) {
+  const iconContainer = document.getElementById(containerId)
+  const Unicode = [
+    "\uf001", //musica
+    "\uf1e2", //bomba
+    "\uf0c2", //nuvem
+    "\uf004", //coração
+    "\uf005", //estrela
+    "\uf118", //carinha feliz
+    "\uf007", //boneco
+    "\uf48b", //caminhao veloz
+    "\uf6e2", //fantasma
+    "\uf64f", //cidade
+    "\uf206", //bicicleta
+    "\uf578", //peixe
+    "\uf013", //engrenagem
+    "\uf06d", //fogo
+    "\uf015", //casa
+    "\uf188", //bug
+    "\uf017", //relogio
+    "\uf21b", //espiao
+    "\uf11e", //bandeira quadriculada
+    "\uf072", //avião
+    "\uf0d6", //dinheiro
+    "\uf084", //chave
+    "\uf11b", //gamepad
+    "\uf5e4", //carro
+    "\uf135", //foguete
+    "\uf21c", //moto
+    "\uf5dc", //cerebro
+    "\uf091", //trofeu
+    "\uf544", //robo
+    "\uf521", //coroa
+    "\ue074", //virus
+    "\uf63b", //monster truck
+    "\uf51e", //moedas
+    "\uf05b", //mira
+    "\uf0eb", //lampada
+    "\uf12e", //quebra-cabeça
+    "\uf522", //dados
+    "\uf5b6", //van
+    "\uf45f", //bola de volei
+    "\uf63c", //camioneta
+    "\uf4df", //caminhao
+    "\uf722", //trator
+    "\uf45d", //tenis de mesa
+    "\uf197", //aviao espacial
+    "\uf753", //metero
+    "\ue518", //jato
+    "\uf533", //helicoptero
+    "\ue19b", //arma
+    "\uf1e3", //bola de futebol
+    "\uf7ba", //radiacao
+  ]
+
+  for (let i = 0; i < 70; i++) {
+    const icon = document.createElement("i")
+    icon.className = "icon fas"
+    icon.innerHTML = randomIcon(Unicode)
+    iconContainer.appendChild(icon)
+  }
+}
+
+// function to generate a random fontawesome unicode value
+function randomIcon(values) {
+  const randomIndex = Math.floor(Math.random() * values.length)
+  return values[randomIndex]
+}
+
 let menu = document.querySelector(".menu-icon")
 let navbar = document.querySelector(".menu")
 
 menu.onclick = () => {
-  navbar.classList.toggle("active");
-  menu.classList.toggle('move');
-  bell.classList.remove('active');
+  navbar.classList.toggle("active")
+  menu.classList.toggle("move")
+  bell.classList.remove("active")
 }
 
 // Notfication
-let bell = document.querySelector('.notification');
+let bell = document.querySelector(".notification")
 
-document.querySelector('#bell-icon').onclick = () => {
-  bell.classList.toggle('active');
+document.querySelector("#bell-icon").onclick = () => {
+  bell.classList.toggle("active")
 }
 
 // Swipper
